1c80436148e2599f62329cc791c3461db9d6d737
qetyip0123 <qetyip0123@gmail.com>
2023-10-21 18:50:37 +0700

    content update

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -4,17 +4,19 @@ import DataTablesCore from 'datatables.net';
 DataTable.use(DataTablesCore);
 
 const columns = [
-  { data: 'level', "width": "5%"},
-  { data: 'title', "width": "35%", render: (data, type, row, meta) => `<a target="_blank" href="http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&bmsmd5=${row.md5}">${data}</a>` },
+  { data: 'level', "width": "5%" },
+  { data: 'title', "width": "35%", render: (data, type, row, meta) => `<a target="_blank" href="http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&${row.md5 ? ('bmsmd5=' + row.md5) : ('bmsid=' + row.bmsid)}">${data}</a>` },
   { data: 'artist', "width": "20%", render: (data, type, row, meta) => `<a target="_blank" href="${row.url}">${data}</a>` },
   { data: 'chart', "width": "5%", render: (data, type, row, meta) => row.url_diff ? `<a target="_blank" href="${row.url_diff}">DL</a>` : `` },
   { data: 'comment', "width": "35%" }
 ];
+
+const url = (import.meta.env.PROD ? '/lr2-mypage' : '') + '/double_time.json'
 </script>
 
 <template>
   <div class="table">
-    <DataTable :ajax="{url:'/double_time.json','dataSrc':''}" :columns="columns" class="display">
+    <DataTable :ajax="{ url: url, 'dataSrc': '' }" :columns="columns" class="display">
       <thead>
         <tr>
           <th>Level</th>
